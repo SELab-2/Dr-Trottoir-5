2c29c752c39c6bc52422deb13e79c4f866c276fb
novdamme <noah-vd@hotmail.com>
2023-03-03 20:41:06 +0100

    #20 trash container model + app in settings
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/trashcontainers/models.py
+++ backend/trashcontainers/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class TrashContainer(models.Model):
+    class TrashType(models.TextChoices):
+        PMD = "PM", "PMD"
+        REST = "RE", "REST"
+        PK = "PK", "PK"
+        GLAS = "GL", "GLAS"
+        GFT = "GF", "GFT"
+
+    class WeekDay(models.TextChoices):
+        MONDAY = "MO", "Monday"
+        TUESDAY = "TU", "Tuesday"
+        WEDNESDAY = "WE", "Wednesday"
+        THURSDAY = "TH", "Thursday"
+        FRIDAY = "FR", "Friday"
+        SATURDAY = "SA", "Saturday"
+        SUNDAY = "SU", "Sunday"
+
+
+    type = models.CharField(
+        max_length=2,
+        choices=TrashType.choices
+    )
+
+    collection_days = ArrayField(
+        models.CharField(
+            max_length=2,
+            choices=WeekDay.choices
+        )
+    )
+    special_actions = models.CharField(
+        default="",
+        max_length=500
+    )
+
+    start_hour = models.TimeField()
+    end_hour = models.TimeField()
