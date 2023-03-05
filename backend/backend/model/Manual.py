f9c8c846a0892e275b98a1848a5c434701d22d12
Kai Hozee <kaihozee@icloud.com>
2023-03-05 14:57:22 +0100

    #21 Made ronde app, added models

--- backend/backend/model/Manual.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Manual(models.Model):
-    file = models.FileField(upload_to='manuals/')
-    fileType = models.TextField()
-
-    # TODO custom of gewoon text based in api
-    status = models.TextField()
