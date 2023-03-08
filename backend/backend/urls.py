9ae62edfa2b13d94e3837b8e52914e526f44599e
pjotrvisman <vismanpjotr@gmail.com>
2023-03-08 20:26:33 +0100

    Undo crud view and urls to avoid merge conflicts

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -40,5 +40,4 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('trashcontainers/', include('trashcontainers.urls'))
 ]
--- backend/trashcontainers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.trashcontainers),
-    path('<int:pk>', views.trashcontainers_pk),
-]
