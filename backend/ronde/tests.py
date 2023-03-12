873829086a7e91dcd2dbf63b85f5cd06fae4e90a
Kai Hozee <kaihozee@icloud.com>
2023-03-13 00:32:05 +0100

    #45 Added extra test

--- backend/ronde/tests.py
+++ backend/ronde/tests.py
@@ -2,14 +2,15 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
-from .views import LocatieEnumListCreateView, LocatieEnumRetrieveDestroyView
-from .models import LocatieEnum
+from .views import LocatieEnumListCreateView, LocatieEnumRetrieveDestroyView, RondeListCreateView
+from .models import LocatieEnum, Ronde
 
 
 class TestApi(TestCase):
 
     def setUp(self):
         LocatieEnum.objects.create(name="Gent")
+        Ronde.objects.create(name="TestRonde", location=LocatieEnum.objects.get(name="Gent"))
 
     def test_add_locatie(self):
         """
@@ -28,4 +29,13 @@ class TestApi(TestCase):
         factory = APIRequestFactory()
         request = factory.get('/api/ronde/locatie/')
         response = LocatieEnumListCreateView.as_view()(request).data
+        self.assertNotEquals(len(response), 0)
+
+    def test_get_rondes(self):
+        """
+            Test for getting Ronde
+        """
+        factory = APIRequestFactory()
+        request = factory.get('/api/ronde/')
+        response = RondeListCreateView.as_view()(request).data
         self.assertNotEquals(len(response), 0)
