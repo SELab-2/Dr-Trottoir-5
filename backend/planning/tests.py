026ed90719773ac0d33386b91fa06563fe95aa77
Robbe Van Rijsselberghe <robbe.vanrijsselberghe@ugent.be>
2023-03-13 19:11:18 +0100

    add all creation tests

--- backend/planning/tests.py
+++ backend/planning/tests.py
@@ -4,6 +4,11 @@ from rest_framework.test import APITestCase, APIRequestFactory
 from .views import *
 from .models import *
 
+from io import BytesIO
+from PIL import Image
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 # Create your tests here.
 
@@ -11,7 +16,9 @@ from .models import *
 class CreateTest(APITestCase):
 
     def setUp(self) -> None:
-        self.wp = WeekPlanning.objects.create(week=0, year=2023).pk
+        self.wp = WeekPlanning.objects.create(week=0, year=2023)
+        self.dp = DagPlanning.objects.create(date="2023-08-31", weekPlanning=self.wp)
+        self.ipb = InfoPerBuilding.objects.create(remark="test", dagPlanning=self.dp)
 
     def testAddWeekPlanning(self):
         factory = APIRequestFactory()
@@ -19,12 +26,45 @@ class CreateTest(APITestCase):
         response = WeekPlanningCLAPIView.as_view()(request).data
         self.assertEqual(response["week"], 12)
         self.assertEqual(response["year"], 2023)
-        self.assertIsNotNone(response["pk"])
+        self.assertIsNotNone(response.get("pk"))
 
     def testAddDagPlanning(self):
         factory = APIRequestFactory()
-        request = factory.post("/api/planning/dagplanning/", {"date": "2023-03-31", "weekPlanning": self.wp})
+        request = factory.post("/api/planning/dagplanning/", {"date": "2023-03-31", "weekPlanning": self.wp.pk})
         response = DagPlanningCreateAndListAPIView.as_view()(request).data
         self.assertEqual(response["date"], "2023-03-31")
-        self.assertEqual(response["weekPlanning"], self.wp)
-        self.assertIsNotNone(response["pk"])
+        self.assertEqual(response["weekPlanning"], self.wp.pk)
+        self.assertIsNotNone(response.get("pk"))
+
+    def testAddInfoPerBuilding(self):
+        factory = APIRequestFactory()
+        request = factory.post("/api/planning/infoperbuilding/", {
+            "remark": "This is a test remark",
+            "dagPlanning": self.dp.pk
+        })
+        response = InfoPerBuildingCLAPIView.as_view()(request).data
+        self.assertEqual(response["remark"], "This is a test remark")
+        self.assertEqual(response["dagPlanning"], self.dp.pk)
+        self.assertIsNotNone(response.get("pk"))
+
+    def testAddBuildingPicture(self):
+        file = BytesIO()
+        image = Image.new('RGBA', size=(100, 100), color=(155, 0, 0))
+        image.save(file, 'png')
+        file.name = 'test.png'
+        file.seek(0)
+
+        factory = APIRequestFactory()
+        request = factory.post("/api/planning/buildingpicture/", {
+            "pictureType": "ST",
+            "image": file,
+            "time": "2002-03-27 22:33",
+            "remark": "testRemark",
+            "infoPerBuilding": self.ipb.pk
+        })
+        response = BuildingPictureCreateAndListAPIView.as_view()(request).data
+        self.assertEqual(response["pictureType"], "ST")
+        self.assertIsNotNone(response.get("image"))
+        self.assertEqual(response["time"], "2002-03-27T22:33:00+01:00")
+        self.assertEqual(response["remark"], "testRemark"),
+        self.assertEqual(response["infoPerBuilding"], self.ipb.pk)
