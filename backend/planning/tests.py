95146d86afad37a7abeca4c956cd2f1e7e741190
Robbe Van Rijsselberghe <robbe.vanrijsselberghe@ugent.be>
2023-03-13 17:46:56 +0100

    start basic creation tests

--- backend/planning/tests.py
+++ backend/planning/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
+from rest_framework.test import APITestCase, APIRequestFactory
+from .views import *
+from .models import *
+
+
 # Create your tests here.
+
+
+class CreateTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.wp = WeekPlanning.objects.create(week=0, year=2023).pk
+
+    def testAddWeekPlanning(self):
+        factory = APIRequestFactory()
+        request = factory.post("/api/planning/weekplanning/", {"week": 12, "year": 2023})
+        response = WeekPlanningCLAPIView.as_view()(request).data
+        self.assertEqual(response["week"], 12)
+        self.assertEqual(response["year"], 2023)
+        self.assertIsNotNone(response["pk"])
+
+    def testAddDagPlanning(self):
+        factory = APIRequestFactory()
+        request = factory.post("/api/planning/dagplanning/", {"date": "2023-03-31", "weekPlanning": self.wp})
+        response = DagPlanningCreateAndListAPIView.as_view()(request).data
+        self.assertEqual(response["date"], "2023-03-31")
+        self.assertEqual(response["weekPlanning"], self.wp)
+        self.assertIsNotNone(response["pk"])
