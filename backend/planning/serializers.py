7be6b62a9f6b3e9e6f6b9d8421748f7f534bbdcb
Robbe Van Rijsselberghe <robbe.vanrijsselberghe@ugent.be>
2023-03-13 20:20:27 +0100

    remove create methods, default are good enough

--- backend/planning/serializers.py
+++ backend/planning/serializers.py
@@ -13,20 +13,6 @@ class DagPlanningSerializer(serializers.ModelSerializer):
             "pk"
         ]
 
-    def create(self, validated_data):
-        date = validated_data['date']
-        # student = validated_data['student']
-        # ronde = validated_data['ronde']
-        weekplanning = validated_data["weekPlanning"]
-        dagplanning = DagPlanning(
-            date=date,
-            weekPlanning=weekplanning,
-            # ronde=ronde,
-            # student=student
-        )
-        dagplanning.save()
-        return dagplanning
-
 
 class BuildingPictureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -40,17 +26,6 @@ class BuildingPictureSerializer(serializers.ModelSerializer):
             "pk"
         ]
 
-    def create(self, validated_data):
-        pictureType = validated_data["pictureType"]
-        image = validated_data["image"]
-        time = validated_data['time']
-        remark = validated_data['remark']
-        infoPerBuilding = validated_data["infoPerBuilding"]
-        buildingPicture = BuildingPicture(pictureType=pictureType, image=image, time=time, remark=remark,
-                                          infoPerBuilding=infoPerBuilding)
-        buildingPicture.save()
-        return buildingPicture
-
 
 class InfoPerBuildingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -61,13 +36,6 @@ class InfoPerBuildingSerializer(serializers.ModelSerializer):
             "pk"
         ]
 
-    def create(self, validated_data):
-        dagPlanning = validated_data["dagPlanning"]
-        remark = validated_data["remark"]
-        infoPerBuilding = InfoPerBuilding(dagPlanning=dagPlanning, remark=remark)
-        infoPerBuilding.save()
-        return infoPerBuilding
-
 
 class WeekPlanningSerializer(serializers.ModelSerializer):
     class Meta:
@@ -77,10 +45,3 @@ class WeekPlanningSerializer(serializers.ModelSerializer):
             "year",
             "pk"
         ]
-
-    def create(self, validated_data):
-        week = validated_data['week']
-        year = validated_data['year']
-        weekPlanning = WeekPlanning(week=week, year=year)
-        weekPlanning.save()
-        return weekPlanning
