64b15c118a64b21c60edb4b32075ae60ef4fd4bc
novdamme <noah-vd@hotmail.com>
2023-03-15 17:22:00 +0100

    user has to be authenticated for readonly
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class ReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        return request.method in permissions.SAFE_METHODS and not request.user.is_anonymous
 
 
 class AdminPermission(permissions.BasePermission):
