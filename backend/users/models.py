8090eede8f0106be1f6d3b21bbea49e1c5f2a3ec
novdamme <noah-vd@hotmail.com>
2023-03-16 14:06:58 +0100

    #57 ascii_letters for more variation
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/users/models.py
+++ backend/users/models.py
@@ -117,7 +117,7 @@ class User(AbstractUser):
         """
             Create new otp when user is saved
         """
-        self.otp = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(25))
+        self.otp = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(25))
         super().save(*args, **kwargs)
 
 
