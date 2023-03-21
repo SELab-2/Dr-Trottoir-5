f41556b6ec43e9a6af2aad9dc0581914a796628e
novdamme <noah-vd@hotmail.com>
2023-03-21 16:45:37 +0100

    #73 changed authentication to rest_framework_simplejwt with refresh tokens etc.
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 from rest_framework import serializers, generics
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from .permissions import AdminPermission, SuperstudentPermission, ReadOnly
+from .permissions import AdminPermission, SuperstudentPermission
 from .serializers import RegistrationSerializer, RoleAssignmentSerializer, UserSerializer
 
 
@@ -31,18 +31,12 @@ def registration_view(request):
             )
             data['email'] = request.data['email']
             data['name'] = request.data['first_name'] + " " + request.data['last_name']
-            data['token'] = Token.objects.get(user=user).key
+            refresh = RefreshToken.for_user(user)
+            data['refresh'] = str(refresh)
+            data['access'] = str(refresh.access_token)
         else:
             data = serializer.errors
         return Response(data)
-
-
-@api_view(["GET"])
-@permission_classes([ReadOnly])
-def logout_view(request):
-    request.user.auth_token.delete()
-    return Response('User Logged out successfully')
-
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
