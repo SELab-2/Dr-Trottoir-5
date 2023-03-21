6726a48e73085d35961427fb5455bebe687c7a10
jbvilla <jonasbyle12@gmail.com>
2023-03-21 22:36:38 +0100

    update style card

--- frontend/src/components/student/fotoCardStudent.vue
+++ frontend/src/components/student/fotoCardStudent.vue
@@ -1,22 +1,31 @@
 <template>
   <v-container fluid>
-    <v-card max-width="600px" min-width="345">
+    <v-card max-width="500px" min-width="345">
       <v-card-text>
         <v-row align-end>
           <v-col>
             <p>{{ description }}</p>
           </v-col>
-          <v-col align="right">
-            <v-img :src="imageURL" :max-width="mobile ? '100%' : '250'" :max-height="250"></v-img>
+          <v-col class="d-flex align-center">
+            <v-row justify="center">
+              <v-img :src="imageURL" :max-width="mobile ? '100%' : '250'" :max-height="150"></v-img>
+            </v-row>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row align="end">
           <v-col>
-            <p>{{ timeStamp }}</p>
+            <p style="font-size: 8px">{{ timeStamp }}</p>
           </v-col>
-          <v-col align="right">
-            <v-btn v-on:click="goToEditPage" class="button-margin" color="primary" text style="font-size: 45%;">Pas aan</v-btn>
-            <v-btn v-on:click="deletePost" color="primary" text style="font-size: 45%;" >Verwijder</v-btn>
+          <v-col>
+            <v-row justify="center">
+                <v-btn v-on:click="goToEditPage" class="button-margin mr-2" color="primary" text
+                       style="font-size: 45%;">
+                  Pas aan
+                </v-btn>
+                <v-btn v-on:click="deletePost" color="primary" text style="font-size: 45%;">
+                  Verwijder
+                </v-btn>
+            </v-row>
           </v-col>
         </v-row>
       </v-card-text>
