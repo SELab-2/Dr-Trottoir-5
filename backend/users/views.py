668cea61f528686e185cf88eccf8623783869064
novdamme <noah-vd@hotmail.com>
2023-03-23 20:34:36 +0100

    linter fix
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework import serializers, generics
 from django.core.mail import send_mail
 from django.conf import settings
