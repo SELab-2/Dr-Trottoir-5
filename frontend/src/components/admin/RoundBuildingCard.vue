d04d4203e7db70ffc2419e3934e10c1de20daace
jbvilla <jonasbyle12@gmail.com>
2023-03-24 13:30:14 +0100

    lay-out fix

--- frontend/src/components/admin/RoundBuildingCard.vue
+++ frontend/src/components/admin/RoundBuildingCard.vue
@@ -1,17 +1,28 @@
 <template>
   <v-container class="container-border">
-    <v-row>
-      <v-col cols="3" class="d-flex align-center">
+    <v-row align="center">
+      <v-col cols="3">
         <p>{{ name }}</p>
       </v-col>
-      <v-col cols="3" class="d-flex align-center">
+      <v-col cols="2">
         <p>{{ adres }}</p>
       </v-col>
-      <v-col/>
-      <v-col cols="3" class="d-flex align-center justify-end">
-        <v-btn icon tile class="button-margin" style="max-height: 35px; max-width: 35px;" v-on:click="goToEditPage">
-          <img :src="require('@/assets/edit.png')" alt="Button Image" style="max-height: 35px; max-width: 35px;"/>
-        </v-btn>
+      <v-col cols="1"/>
+      <v-col cols="1">
+        <v-select
+          :items="documentActions"
+          v-model="selectedActions"
+        ></v-select>
+      </v-col>
+      <v-col cols="1"/>
+      <v-col cols="1">
+        <v-select
+          :items="documentStatus"
+          v-model="selectedStatus"
+        ></v-select>
+      </v-col>
+      <v-col cols="1"/>
+      <v-col cols="2" class="text-right">
         <v-btn icon tile style="max-height: 35px; max-width: 35px;" v-on:click="deletePost">
           <img :src="require('@/assets/delete.png')" alt="Button Image" style="max-height: 35px; max-width: 35px"/>
         </v-btn>
@@ -26,6 +37,17 @@ export default {
   props: {
     name: { type: String, default: 'Empty' },
     adres: { type: String, default: 'Empty' }
+  },
+  data: () => ({
+    documentActions: ['Type 1', 'Type 2', 'Type 3'],
+    selectedActions: null,
+    documentStatus: ['Option 1', 'Option 2', 'Option 3'],
+    selectedStatus: null // TODO
+  }),
+  methods: {
+    deletePost: function () {
+      // TODO
+    }
   }
 }
 </script>
