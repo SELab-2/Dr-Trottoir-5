8e5a2525be6ed53bb95c2e186c13d2c4a62a079b
Robbe Van Rijsselberghe <robbevanrijsselberghe@gmail.com>
2023-03-26 23:22:22 +0200

    exception handling (done ?)

--- backend/planning/views.py
+++ backend/planning/views.py
@@ -1,99 +1,236 @@
 from rest_framework import generics
 from .serializers import *
-from users.permissions import StudentReadOnly, AdminPermission, SuperstudentPermission, StudentPermission
+from users.permissions import StudentReadOnly, AdminPermission, \
+    SuperstudentPermission, StudentPermission
+from django.contrib.auth import get_user_model
+from ronde.models import Ronde
+from .models import *
+
+import datetime
 
 
 class DagPlanningCreateAndListAPIView(generics.ListCreateAPIView):
     queryset = DagPlanning.objects.all()
     serializer_class = DagPlanningSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
     def post(self, request, *args, **kwargs):
-
-        try:
-            WeekPlanning.objects.get(pk=request.data["weekPlanning"])
-        except WeekPlanning.DoesNotExist:
-            raise serializers.ValidationError(
+        data = request.data
+        errors = []
+        if data.get("weekPlanning") is None:
+            errors.append(
                 {
-                    "errors": [
-                        {
-                            "message": "referenced pk not in db",
-                            "field": "weekPlanning"
-                        }
-                    ]
-                }, code='invalid')
+                    "message": "field is required",
+                    "field": "weekPlanning"
+                }
+            )
+        else:
+            try:
+                WeekPlanning.objects.get(pk=request.data["weekPlanning"])
+            except (WeekPlanning.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "weekPlanning"
+                })
+
+        if data.get("date") is None:
+            errors.append({
+                "message": "field is required",
+                "field": "date"
+            })
+        else:
+            try:
+                datetime.datetime.strptime(data["date"], "%Y-%m-%d")
+            except ValueError:
+                errors.append({
+                    "message": "date has the wrong format, use YYYY-MM-DD",
+                    "field": "date"
+                })
+        if data.get("student") is None:
+            errors.append({
+                "message": "field is required",
+                "field": "student"
+            })
+        else:
+            try:
+                get_user_model().objects.get(pk=data["student"])
+            except (get_user_model().DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "student"
+                })
+
+        if data.get("ronde") is None:
+            errors.append({
+                "message": "field is required",
+                "field": "ronde"
+            })
+        else:
+            try:
+                Ronde.objects.get(pk=data["ronde"])
+            except (Ronde.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "reference pk not in db",
+                    "field": "ronde"
+                })
+
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
         return super().post(request=request, args=args, kwargs=kwargs)
 
 
-class DagPlanningRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+class DagPlanningRetrieveUpdateDestroyAPIView(
+    generics.RetrieveUpdateDestroyAPIView):
     queryset = DagPlanning.objects.all()
     serializer_class = DagPlanningSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
 
 class BuildingPictureCreateAndListAPIView(generics.ListCreateAPIView):
     queryset = BuildingPicture.objects.all()
     serializer_class = BuildingPictureSerializer
-    permission_classes = [StudentPermission | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentPermission | AdminPermission | SuperstudentPermission]
+
     # TODO: a user can only see the pictures that he added (?)
 
     def post(self, request, *args, **kwargs):
-        try:
-            InfoPerBuilding.objects.get(pk=request.data["infoPerBuilding"])
-        except InfoPerBuilding.DoesNotExist:
-            raise serializers.ValidationError(
-                {
-                    "errors": [
-                        {
-                            "message": "referenced pk not in db", "field": "infoPerBuilding"
-                        }
-                    ]
-                }, code='invalid')
+        data = request.data
+        errors = []
+
+        if "pictureType" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "pictureType"
+            })
+        elif data["pictureType"] not in BuildingPicture.PictureEnum.values:
+            errors.append({
+                "message": "not a valid choice",
+                "field": "pictureType"
+            })
+        if "image" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "image"
+            })
+        elif data["image"] not in request.FILES.getlist("image"):
+            errors.append({
+                "message": "submitted data was not a file",
+                "field": "image"
+            })
+        if "time" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "time"
+            })
+        else:
+            try:
+                datetime.datetime.strptime(data["time"], "%Y-%m-%dT%H:%M")
+            except ValueError:
+                errors.append({
+                    "message": "time has wrong format, try YYYY-MM-DDThh:mm",
+                    "field": "time"
+                })
+        if "infoPerBuilding" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "infoPerBuilding"
+            })
+        else:
+            try:
+                InfoPerBuilding.objects.get(pk=request.data["infoPerBuilding"])
+            except (InfoPerBuilding.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "infoPerBuilding"
+                })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
         return super().post(request=request, args=args, kwargs=kwargs)
 
 
 class BuildingPictureRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BuildingPicture.objects.all()
     serializer_class = BuildingPictureSerializer
-    permission_classes = [StudentPermission | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentPermission | AdminPermission | SuperstudentPermission]
     # TODO: only the user that created a BuildingPicture should be able to update it
 
 
 class InfoPerBuildingCLAPIView(generics.ListCreateAPIView):
     queryset = InfoPerBuilding.objects.all()
     serializer_class = InfoPerBuildingSerializer
-    permission_classes = [StudentPermission | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentPermission | AdminPermission | SuperstudentPermission]
+
     # TODO: a user can only see the info per building that he added (?)
 
     def post(self, request, *args, **kwargs):
-        try:
-            DagPlanning.objects.get(pk=request.data["dagPlanning"])
-        except DagPlanning.DoesNotExist:
-            raise serializers.ValidationError(
-                {
-                    "errors": [
-                        {
-                            "message": "referenced pk not in db", "field": "dagPlanning"
-                        }
-                    ]
-                }, code='invalid')
+        data = request.data
+        errors = []
+        if "dagPlanning" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "dagPlanning"
+            })
+        else:
+            try:
+                DagPlanning.objects.get(pk=request.data["dagPlanning"])
+            except (DagPlanning.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "dagPlanning"
+                })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
         return super().post(request=request, args=args, kwargs=kwargs)
 
 
 class InfoPerBuildingRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = InfoPerBuilding.objects.all()
     serializer_class = InfoPerBuildingSerializer
-    permission_classes = [StudentPermission | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentPermission | AdminPermission | SuperstudentPermission]
     # TODO: only the user that created an InfoPerBuilding should be able to update it
 
 
 class WeekPlanningCLAPIView(generics.ListCreateAPIView):
     queryset = WeekPlanning.objects.all()
     serializer_class = WeekPlanningSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
+
+    def post(self, request, *args, **kwargs):
+
+        data = request.data
+        errors = []
+        if data.get("week") is None:
+            errors.append({
+                "message": "field is required",
+                "field": "week"
+            })
+        if data.get("year") is None:
+            errors.append({
+                "message": "field is required",
+                "field": "year"
+            })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
+        return super().post(request, *args, **kwargs)
 
 
 class WeekPlanningRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = WeekPlanning.objects.all()
     serializer_class = WeekPlanningSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
--- backend/ronde/views.py
+++ backend/ronde/views.py
@@ -1,17 +1,19 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, serializers
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 import os
-from django.conf import settings
-from .models import LocatieEnum, Manual, Building, Ronde
-from .serializers import LocatieEnumSerializer, ManaulSerializer, BuildingSerializer, RondeSerializer
-from users.permissions import StudentReadOnly, AdminPermission, SuperstudentPermission
+from .models import *
+from .serializers import LocatieEnumSerializer, ManualSerializer, \
+    BuildingSerializer, RondeSerializer
+from users.permissions import StudentReadOnly, AdminPermission, \
+    SuperstudentPermission
 
 
 class LocatieEnumListCreateView(generics.ListCreateAPIView):
     queryset = LocatieEnum.objects.all()
     serializer_class = LocatieEnumSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
     def create(self, request, *args, **kwargs):
         """
@@ -21,18 +23,43 @@ class LocatieEnumListCreateView(generics.ListCreateAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response({"succes": serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"succes": serializer.data},
+                            status=status.HTTP_201_CREATED)
         elif "name" not in request.data:
-            return Response({"error": ["Er is geen veld 'name' meegegeven"]}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": ["Er is geen veld 'name' meegegeven"]},
+                            status=status.HTTP_400_BAD_REQUEST)
         elif request.data["name"] == "":
-            return Response({"error": "Het veld 'name' is leeg"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Het veld 'name' is leeg"},
+                            status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        errors = []
+        if "name" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "name"
+            })
+
+        elif data["name"] == "":
+            errors.append({
+                "message": "field can't be empty",
+                "field": "name"
+            })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
+        return super().post(request, *args, **kwargs)
 
 
 class LocatieEnumRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     serializer_class = LocatieEnumSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
     """
         View that deletes and gets a specific Location
     """
@@ -44,9 +71,10 @@ class LocatieEnumRetrieveDestroyView(generics.RetrieveDestroyAPIView):
 
 class ManualListCreateView(generics.ListCreateAPIView):
     queryset = Manual.objects.all()
-    serializer_class = ManaulSerializer
+    serializer_class = ManualSerializer
     parser_classes = (MultiPartParser, FormParser)
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
     def create(self, request, *args, **kwargs):
         """
@@ -55,28 +83,75 @@ class ManualListCreateView(generics.ListCreateAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response({"succes": serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"succes": serializer.data},
+                            status=status.HTTP_201_CREATED)
         elif "file" not in request.data or "fileType" not in request.data or "manualStatus" not in request.data:
             return Response(
-                {"error": ["Een van de benodigde velden: 'file', 'fileType' of 'manualStatus is niet aanwezig"]},
+                {"error": [
+                    "Een van de benodigde velden: 'file', 'fileType' of 'manualStatus is niet aanwezig"]},
                 status=status.HTTP_400_BAD_REQUEST)
-        elif request.data["file"] == "" or request.data["fileType"] == "" or request.data["manualStatus"] == "":
+        elif request.data["file"] == "" or request.data["fileType"] == "" or \
+                request.data["manualStatus"] == "":
             return Response(
-                {"error": ["Een van de benodigde velden: 'file', 'fileType' of 'manualStatus is leeg"]},
+                {"error": [
+                    "Een van de benodigde velden: 'file', 'fileType' of 'manualStatus is leeg"]},
                 status=status.HTTP_400_BAD_REQUEST)
-        elif request.data["manualStatus"] not in ["Klaar", "Update nodig", "Bezig", "Geüpdatet"]:
+        elif request.data["manualStatus"] not in ["Klaar", "Update nodig",
+                                                  "Bezig", "Geüpdatet"]:
             return Response(
                 {"error": [
                     "Er is een foute manual status meegegeven. Kies uit volgende opties: Klaar, Update nodig, Bezig of "
                     "Geüpdatet"]},
                 status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        errors = []
+        if "file" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "file"
+            })
+        elif data["file"] not in request.FILES.getlist("file"):
+            errors.append({
+                "message": "submitted data was not a file",
+                "field": "file"
+            })
+        if "fileType" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "fileType"
+            })
+        elif data["fileType"] == "":
+            errors.append({
+                "message": "field can't be blank",
+                "field": "fileType"
+            })
+
+        if "manualStatus" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "manualStatus"
+            })
+        elif data["manualStatus"] not in ManualStatusField.values:
+            errors.append({
+                "message": "not a valid choice",
+                "field": "manualStatus"
+            })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
+        return super().post(request, *args, **kwargs)
 
 
 class ManualRetrieveDestroyView(generics.RetrieveDestroyAPIView):
-    serializer_class = ManaulSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    serializer_class = ManualSerializer
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
     """
         View that deletes and gets a specific Location
     """
@@ -85,11 +160,13 @@ class ManualRetrieveDestroyView(generics.RetrieveDestroyAPIView):
         id = self.kwargs['pk']
         manual = Manual.objects.filter(id=id)
         if len(manual) == 0:
-            return Response({"error": [f"Manual met {id} is niet aanwezig"]}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": [f"Manual met {id} is niet aanwezig"]},
+                            status=status.HTTP_400_BAD_REQUEST)
         for man in manual:
             os.remove(os.path.join(settings.MEDIA_ROOT, man.file.name))
             man.delete()
-        return Response({"succes": ["Manual is verwijdert"]}, status=status.HTTP_200_OK)
+        return Response({"succes": ["Manual is verwijdert"]},
+                        status=status.HTTP_200_OK)
 
     def get_queryset(self):
         id = self.kwargs['pk']
@@ -99,12 +176,73 @@ class ManualRetrieveDestroyView(generics.RetrieveDestroyAPIView):
 class BuildingListCreateView(generics.ListCreateAPIView):
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        errors = []
+        if "adres" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "adres"
+            })
+        elif data["adres"] == "":
+            errors.append({
+                "message": "field may not be blank",
+                "field": "adres"
+            })
+        if "ivago_klantnr" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "ivago_klantnr"
+            })
+        else:
+            try:
+                x = int(data["ivago_klantnr"])
+            except Exception:
+                errors.append({
+                    "message": "field must be integer",
+                    "field": "ivago_klantnr"
+                })
+        if "manual" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "manual"
+            })
+        else:
+            try:
+                Manual.objects.get(pk=data["manual"])
+            except (Manual.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "manual"
+                })
+
+        if "location" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "location"
+            })
+        else:
+            try:
+                LocatieEnum.objects.get(pk=data["location"])
+            except (LocatieEnum.DoesNotExist, ValueError):
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "location"
+                })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
+        return super().post(request, *args, **kwargs)
 
 
 class BuildingRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     serializer_class = BuildingSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
     def get_queryset(self):
         id = self.kwargs['pk']
@@ -114,12 +252,55 @@ class BuildingRetrieveDestroyView(generics.RetrieveDestroyAPIView):
 class RondeListCreateView(generics.ListCreateAPIView):
     queryset = Ronde.objects.all()
     serializer_class = RondeSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        errors = []
+
+        if "name" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "name"
+            })
+        if "location" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "location"
+            })
+        else:
+            try:
+                LocatieEnum.objects.get(pk=data["location"])
+            except LocatieEnum.DoesNotExist:
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "location"
+                })
+        if "buildings" not in data:
+            errors.append({
+                "message": "field is required",
+                "field": "buildings"
+            })
+        else:
+            try:
+                Building.objects.get(pk=data["buildings"])
+            except Building.DoesNotExist:
+                errors.append({
+                    "message": "referenced pk not in db",
+                    "field": "buildings"
+                })
+        if len(errors) > 0:
+            raise serializers.ValidationError({
+                "errors": errors
+            })
+        return super().post(request, *args, **kwargs)
 
 
 class RondeRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     serializer_class = RondeSerializer
-    permission_classes = [StudentReadOnly | AdminPermission | SuperstudentPermission]
+    permission_classes = [
+        StudentReadOnly | AdminPermission | SuperstudentPermission]
 
     def get_queryset(self):
         id = self.kwargs['pk']
