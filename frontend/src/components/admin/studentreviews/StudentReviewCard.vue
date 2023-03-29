4fb37dbe1c12f091e76fd270c99cb7c18e2b2025
WouterDeBolle <debollewouter@gmail.com>
2023-03-29 19:22:12 +0200

    add header

---
+++ frontend/src/components/admin/studentreviews/StudentReviewCard.vue
@@ -0,0 +1,74 @@
+<!--
+Needs to get a data object with a name and then three int values as time in minutes
+The time automatically will shown as the "hh:mm" format
+-->
+
+<template>
+  <v-container class="container-border align-center">
+    <v-row align="stretch">
+      <v-col cols="3"  class="column" >
+        <p>{{ this.args.name }}</p>
+      </v-col>
+      <v-col cols="3" class="column">
+        <p>{{ time1 }}</p>
+      </v-col>
+      <v-col cols="3" class="column">
+        <p>{{ time2 }}</p>
+      </v-col>
+      <v-col cols="3" class="column">
+        <p>{{ time3 }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+function toTimeString (time) {
+  let hours = Math.floor(time / 60)
+  if (hours < 10) { hours = '0' + hours }
+  let min = time % 60
+  if (min < 10) { min = '0' + min }
+  return hours + ':' + min
+}
+
+export default {
+  name: 'TrashPickupCard',
+  props: {
+    args: {
+      default: {
+        name: 'name',
+        first: 0,
+        second: 0,
+        third: 0
+      }
+    }
+  },
+  computed: {
+    time1 () {
+      return toTimeString(this.args.first)
+    },
+    time2 () {
+      return toTimeString(this.args.second)
+    },
+    time3 () {
+      return toTimeString(this.args.third)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-border{
+  border: 1px solid #E3e3e3;
+  border-radius: 25px;
+}
+
+.column{
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  text-align: center;
+}
+
+</style>
