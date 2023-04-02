e870566cdf90b51ed081a73438eefc85ec02005a
jbvilla <jonasbyle12@gmail.com>
2023-04-02 17:32:26 +0200

    change FotoCardStudent so it is centered

--- frontend/src/components/student/OverviewScreenStudentPosts.vue
+++ frontend/src/components/student/OverviewScreenStudentPosts.vue
@@ -1,16 +1,27 @@
 <template>
+  <v-row >
+    <v-col align="right">
+        <v-avatar class="own-button-margin" >
+          <v-icon v-on:click="goToFotoPage" dark size="30px" color="black">mdi-camera-outline</v-icon>
+        </v-avatar>
+    </v-col>
+  </v-row>
+
+  <h1 align="center">Resto s5</h1>
+
   <v-col align="center">
-    <h1>Resto s5</h1>
-    <v-btn icon tile class="button-margin" style="max-height: 35px; max-width: 35px;" v-on:click="goToEditPage">
-    </v-btn>
     <h3>Aankomst</h3>
   </v-col>
 
-  <FotoCardStudent/>
-  <FotoCardStudent/>
+  <div align="center">
+    <FotoCardStudent/>
+    <FotoCardStudent/>
+    <FotoCardStudent/>
+    <FotoCardStudent/>
+    <FotoCardStudent/>
+  </div>
+
 </template>
-
-
 
 <script>
 import FotoCardStudent from "@/components/student/FotoCardStudent.vue";
@@ -23,10 +34,18 @@ export default {
       type: Object,
       default: () => ({name: 'Empty', argsCount: 0})
     }
-  }
+  },
+  methods: {
+    goToFotoPage: function () {
+      // TODO
+    }
+  },
 }
 </script>
 
 <style scoped>
-
+.own-button-margin {
+  margin-right: 5px;
+  margin-top: 5px;
+}
 </style>
