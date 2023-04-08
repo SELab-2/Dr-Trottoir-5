53de26bbc8a4a905edd78e29e64de952729c2fd9
novdamme <noah-vd@hotmail.com>
2023-04-08 16:48:04 +0200

    #193 add/delete/edit van trashcontainers allemaal in één view
    Signed-off-by: novdamme <noah-vd@hotmail.com>

--- backend/trashtemplates/urls.py
+++ backend/trashtemplates/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TrashContainerTemplateListCreateView.as_view()),
-    path('<int:template_id>/add/trashcontainer/', views.add_trash_container_view),
-    path('<int:template_id>/delete/trashcontainer/', views.delete_trash_container_view),
-    path('<int:template_id>/edit/trashcontainer/', views.edit_trash_container_view)
+    path('<int:template_id>/trashcontainers/', views.trash_container_view)
 ]
