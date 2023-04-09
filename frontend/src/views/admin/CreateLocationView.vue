b3388e706a6204920df61474f03f448f3bac6434
Kai Hozee <kaihozee@icloud.com>
2023-04-09 18:35:24 +0200

    Styling

--- frontend/src/views/admin/CreateLocationView.vue
+++ frontend/src/views/admin/CreateLocationView.vue
@@ -1,15 +1,20 @@
 <template>
-  <v-row>
-    <v-col>
-      <h1>Naam</h1>
-    </v-col>
-    <v-col>
-      <v-text-field variant="outlined" v-model="name"></v-text-field>
-    </v-col>
-    <v-col>
-      <normal-button text="Voeg Locatie toe" :parent-function="addLocation"></normal-button>
-    </v-col>
+  <v-row justify="center" align="center" class="pt-10">
+    <h1>Nieuwe locatie aanmaken</h1>
   </v-row>
+  <v-card color="white" class="mx-auto my-16 w-75">
+    <v-row>
+      <v-col lg="6" md="6" class="d-flex align-center justify-end pt-10">
+        <h1>Naam</h1>
+      </v-col>
+      <v-col lg="6" md="6" class="d-flex align-center justify-start">
+        <v-text-field class="text_field" variant="outlined" v-model="name"></v-text-field>
+      </v-col>
+      <v-col md="12" lg="12" class="d-flex align-center justify-center pt-10 pb-10">
+        <normal-button text="Voeg Locatie toe" :parent-function="addLocation"></normal-button>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
@@ -22,7 +27,7 @@ export default {
   components: {NormalButton},
   data: () => {
     return {
-      name: ''
+      name: '',
     }
   },
   methods: {
@@ -44,3 +49,11 @@ export default {
   }
 }
 </script>
+<style>
+.text_field {
+  height: 40px;
+  max-width: 350px;
+  padding-left: 5px;
+  padding-top: 5px;
+}
+</style>
