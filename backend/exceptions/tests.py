28bb903e94d826900ec331adc39f30ce5b35c699
Robbe Van Rijsselberghe <robbevanrijsselberghe@gmail.com>
2023-04-11 13:32:37 +0200

    exceptionhandler split functionality finished, split required and non required error handling

--- backend/exceptions/tests.py
+++ backend/exceptions/tests.py
@@ -37,10 +37,20 @@ class ExceptionHandlerTest(TestCase):
 
     def test_enum_value_success(self):
         handler = ExceptionHandler()
+        self.assertTrue(handler.check_enum_value("MO", "name",
+                                                 PickUpDay.WeekDayEnum.values))
+
+    def test_enum_value_required_success(self):
+        handler = ExceptionHandler()
         self.assertTrue(handler.check_enum_value_required("MO", "name",
                                                           PickUpDay.WeekDayEnum.values))
 
-    def test_enum_value_fail_none(self):
+    def test_enum_value_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_enum_value(None, "name",
+                                                 PickUpDay.WeekDayEnum.values))
+
+    def test_enum_value_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_enum_value_required(None, "name",
                                                            PickUpDay.WeekDayEnum.values))
@@ -48,65 +58,136 @@ class ExceptionHandlerTest(TestCase):
 
     def test_enum_value_fail_bad_value(self):
         handler = ExceptionHandler()
+        self.assertFalse(handler.check_enum_value("Bad Value", "name",
+                                                  PickUpDay.WeekDayEnum.values))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_enum_value_required_fail_bad_value(self):
+        handler = ExceptionHandler()
         self.assertFalse(handler.check_enum_value_required("Bad Value", "name",
                                                            PickUpDay.WeekDayEnum.values))
         self.assertRaises(ValidationError, handler.check)
 
     def test_time_value_success(self):
         handler = ExceptionHandler()
+        self.assertTrue(handler.check_time_value("07:05", "name"))
+
+    def test_time_value_required_success(self):
+        handler = ExceptionHandler()
         self.assertTrue(handler.check_time_value_required("07:05", "name"))
 
-    def test_time_value_fail_none(self):
+    def test_time_value_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_time_value(None, "name"))
+
+    def test_time_value_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_time_value_required(None, "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_time_value_fail_bad_value(self):
         handler = ExceptionHandler()
+        self.assertFalse(handler.check_time_value("42", "name"))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_time_value_required_fail_bad_value(self):
+        handler = ExceptionHandler()
         self.assertFalse(handler.check_time_value_required("42", "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_date_value_success(self):
         handler = ExceptionHandler()
-        self.assertTrue(handler.check_date_value_required("2010-02-15", "name"))
+        self.assertTrue(handler.check_date_value("2010-02-15", "name"))
 
-    def test_date_value_fail_none(self):
+    def test_date_value_required_success(self):
+        handler = ExceptionHandler()
+        self.assertTrue(
+            handler.check_date_value_required("2010-02-15", "name"))
+
+    def test_date_value_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_date_value(None, "name"))
+
+    def test_date_value_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_date_value_required(None, "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_date_value_fail_bad_value(self):
         handler = ExceptionHandler()
-        self.assertFalse(handler.check_date_value_required("2010:05-15", "name"))
+        self.assertFalse(
+            handler.check_date_value("2010:05-15", "name"))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_date_value_required_fail_bad_value(self):
+        handler = ExceptionHandler()
+        self.assertFalse(
+            handler.check_date_value_required("2010:05-15", "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_date_time_value_success(self):
         handler = ExceptionHandler()
-        self.assertTrue(handler.check_date_time_value_required("2010-05-13 07:35",
-                                                      "name"))
+        self.assertTrue(
+            handler.check_date_time_value("2010-05-13 07:35",
+                                          "name"))
 
-    def test_date_time_value_fail_none(self):
+    def test_date_time_value_required_success(self):
+        handler = ExceptionHandler()
+        self.assertTrue(
+            handler.check_date_time_value_required("2010-05-13 07:35",
+                                                   "name"))
+
+    def test_date_time_value_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_date_time_value(None, "name"))
+
+    def test_date_time_value_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_date_time_value_required(None, "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_date_time_value_fail_bad_value(self):
         handler = ExceptionHandler()
-        self.assertFalse(handler.check_date_time_value_required("2010:05:13 07-35",
-                                                       "name"))
+        self.assertFalse(
+            handler.check_date_time_value("2010:05:13 07-35",
+                                          "name"))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_date_time_value_required_fail_bad_value(self):
+        handler = ExceptionHandler()
+        self.assertFalse(
+            handler.check_date_time_value_required("2010:05:13 07-35",
+                                                   "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_primary_key_value_success(self):
         handler = ExceptionHandler()
-        self.assertTrue(handler.check_primary_key_value_required(self.wp.id, "name",
-                                                                 WeekPlanning))
+        self.assertTrue(
+            handler.check_primary_key(self.wp.id, "name", WeekPlanning))
 
-    def test_primary_key_value_fail_none(self):
+    def test_primary_key_value_required_success(self):
         handler = ExceptionHandler()
-        self.assertFalse(handler.check_primary_key_value_required(None, "name", None))
+        self.assertTrue(
+            handler.check_primary_key_value_required(self.wp.id, "name",
+                                                     WeekPlanning))
+
+    def test_primary_key_value_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_primary_key(None, "name", None))
+
+    def test_primary_key_value_required_fail_none(self):
+        handler = ExceptionHandler()
+        self.assertFalse(
+            handler.check_primary_key_value_required(None, "name", None))
         self.assertRaises(ValidationError, handler.check)
 
     def test_primary_key_value_fail_bad_value(self):
+        handler = ExceptionHandler()
+        self.assertFalse(
+            handler.check_primary_key(42, "name", WeekPlanning))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_primary_key_value_required_fail_bad_value(self):
         handler = ExceptionHandler()
         self.assertFalse(
             handler.check_primary_key_value_required(42, "name", WeekPlanning))
@@ -115,15 +196,35 @@ class ExceptionHandlerTest(TestCase):
     def test_primary_key_value_fail_bad_type(self):
         handler = ExceptionHandler()
         self.assertFalse(
-            handler.check_primary_key_value_required("test", "name", WeekPlanning))
+            handler.check_primary_key("test", "name",
+                                      WeekPlanning))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_primary_key_value_required_fail_bad_type(self):
+        handler = ExceptionHandler()
+        self.assertFalse(
+            handler.check_primary_key_value_required("test", "name",
+                                                     WeekPlanning))
         self.assertRaises(ValidationError, handler.check)
 
     def test_file_success(self):
         handler = ExceptionHandler()
         files = MultiValueDict({"name": ["filefield"]})
-        self.assertTrue(handler.check_file_required("filefield", "name", files))
+        self.assertTrue(
+            handler.check_file("filefield", "name", files))
 
-    def test_file_fail_none(self):
+    def test_file_required_success(self):
+        handler = ExceptionHandler()
+        files = MultiValueDict({"name": ["filefield"]})
+        self.assertTrue(
+            handler.check_file_required("filefield", "name", files))
+
+    def test_file_success_none(self):
+        handler = ExceptionHandler()
+        files = MultiValueDict({"name": ["filefield"]})
+        self.assertTrue(handler.check_file(None, "name", files))
+
+    def test_file_required_fail_none(self):
         handler = ExceptionHandler()
         files = MultiValueDict({"name": ["filefield"]})
         self.assertFalse(handler.check_file_required(None, "name", files))
@@ -132,35 +233,71 @@ class ExceptionHandlerTest(TestCase):
     def test_file_fail_bad_value(self):
         handler = ExceptionHandler()
         files = MultiValueDict({"name": ["a file name"]})
-        self.assertFalse(handler.check_file_required("definitely not a file name",
-                                            "name",
-                                                     files))
+        self.assertFalse(
+            handler.check_file("definitely not a file name",
+                                        "name",
+                                        files))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_file_required_fail_bad_value(self):
+        handler = ExceptionHandler()
+        files = MultiValueDict({"name": ["a file name"]})
+        self.assertFalse(
+            handler.check_file_required("definitely not a file name",
+                                        "name",
+                                        files))
         self.assertRaises(ValidationError, handler.check)
 
     def test_integer_success(self):
         handler = ExceptionHandler()
+        self.assertTrue(handler.check_integer(42, "name"))
+
+    def test_integer_required_success(self):
+        handler = ExceptionHandler()
         self.assertTrue(handler.check_integer_required(42, "name"))
 
-    def test_integer_fail_none(self):
+    def test_integer_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_integer(None, "name"))
+
+    def test_integer_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_integer_required(None, "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_integer_fail_bad_value(self):
         handler = ExceptionHandler()
+        self.assertFalse(handler.check_integer("no integer", "name"))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_integer_required_fail_bad_value(self):
+        handler = ExceptionHandler()
         self.assertFalse(handler.check_integer_required("no integer", "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_not_blank_success(self):
         handler = ExceptionHandler()
+        self.assertTrue(handler.check_not_blank("not blank", "name"))
+
+    def test_not_blank_required_success(self):
+        handler = ExceptionHandler()
         self.assertTrue(handler.check_not_blank_required("not blank", "name"))
 
-    def test_not_blank_fail_none(self):
+    def test_not_blank_success_none(self):
+        handler = ExceptionHandler()
+        self.assertTrue(handler.check_not_blank(None, "name"))
+
+    def test_not_blank_required_fail_none(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_not_blank_required(None, "name"))
         self.assertRaises(ValidationError, handler.check)
 
     def test_not_blank_fail_bad_value(self):
+        handler = ExceptionHandler()
+        self.assertFalse(handler.check_not_blank("", "name"))
+        self.assertRaises(ValidationError, handler.check)
+
+    def test_not_blank_required_fail_bad_value(self):
         handler = ExceptionHandler()
         self.assertFalse(handler.check_not_blank_required("", "name"))
         self.assertRaises(ValidationError, handler.check)
