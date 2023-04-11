1f694ac4db9b6a364415727eaa2228ab475dfd51
Robbe Van Rijsselberghe <robbevanrijsselberghe@gmail.com>
2023-04-11 20:36:41 +0200

    PUT PATCH validation to trashcontainer views

--- backend/trashcontainers/views.py
+++ backend/trashcontainers/views.py
@@ -19,9 +19,11 @@ class TrashContainerListCreateView(generics.ListCreateAPIView):
         handler = ExceptionHandler()
         handler.check_enum_value_required(data.get("type"), "type",
                                           TrashContainer.TrashType.values)
-        handler.check_primary_key_value_required(data.get("collection_days"), "collection_days",
+        handler.check_primary_key_value_required(data.get("collection_days"),
+                                                 "collection_days",
                                                  PickUpDay)
-        handler.check_primary_key_value_required(data.get("building"), "building", Building)
+        handler.check_primary_key_value_required(data.get("building"),
+                                                 "building", Building)
         handler.check()
         return super().post(request, *args, **kwargs)
 
@@ -32,13 +34,26 @@ class TrashContainerRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [
         StudentReadOnly | AdminPermission | SuperstudentPermission]
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop("partial", False)
-        kwargs["partial"] = partial
-        return super().update(request, *args, **kwargs)
-
     def put(self, request, *args, **kwargs):
+        data = request.data
+        handler = ExceptionHandler()
+        handler.check_enum_value_required(data.get("type"), "type",
+                                          TrashContainer.TrashType.values)
+        handler.check_primary_key_value_required(data.get("collection_days"),
+                                                 "collection_days",
+                                                 PickUpDay)
+        handler.check_primary_key_value_required(data.get("building"),
+                                                 "building", Building)
+        handler.check()
         return super().put(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
+        data = request.data
+        handler = ExceptionHandler()
+        handler.check_enum_value(data.get("type"), "type",
+                                 TrashContainer.TrashType.values)
+        handler.check_primary_key(data.get("collection_days"),
+                                  "collection_days", PickUpDay)
+        handler.check_primary_key(data.get("building"), "building", Building)
+        handler.check()
         return super().patch(request, *args, **kwargs)
