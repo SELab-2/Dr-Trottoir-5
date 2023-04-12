255a838f280afde3b5ffe232bb5ccf2702fcd675
Kai Hozee <kaihozee@icloud.com>
2023-04-12 20:32:16 +0200

    fixed linter

--- backend/ronde/views.py
+++ backend/ronde/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, serializers
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 import os
