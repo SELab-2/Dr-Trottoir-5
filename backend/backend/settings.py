39a6140ff38084297f11f682f219d9e2fd5e8ec3
WouterDeBolle <debollewouter@gmail.com>
2023-04-17 02:35:20 +0200

    fix missing ,

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://192.168.178.34:8080",
     "http://localhost:8081",
-    "http://192.168.178.34:8081"
+    "http://192.168.178.34:8081",
     "http://127.0.0.1:8080",
 ]
 
