6f2650eb47529747de30e1286418699c4c1a5b0f
Kai Hozee <kaihozee@icloud.com>
2023-04-17 18:33:09 +0200

    Added backend support to add Syndicus

--- frontend/src/components/admin/CreateEditSyndicus.vue
+++ frontend/src/components/admin/CreateEditSyndicus.vue
@@ -26,18 +26,18 @@
         item-value="id"
         v-model="syndicus.location"
       ></v-autocomplete>
-      <label class="black-text">Buildings</label>
+      <label class="black-text">Gebouwen</label>
       <v-autocomplete
         clearable
         outlined
-        :items="this.allBuildings"
+        :items="this.allBuildings.filter(building => building.location === this.syndicus.location)"
         item-title="name"
         item-value="id"
         v-model="syndicus.buildings"
         multiple
       >
         <template v-slot:no-data>
-          <div class="px-4">Er zijn geen gebouwen.</div>
+          <div class="px-4">Er zijn geen gebouwen voor gegeven locatie.</div>
         </template>
       </v-autocomplete>
     </v-form>
@@ -59,6 +59,7 @@ import {RequestHandler} from "@/api/RequestHandler";
 import UserService from "@/api/services/UserService";
 import BuildingService from "@/api/services/BuildingService";
 import LocationService from "@/api/services/LocationService";
+import AuthService from "@/api/services/AuthService";
 
 export default {
   name: 'CreateEditSyndicus',
@@ -82,9 +83,46 @@ export default {
     }
   },
   methods: {
-    addSyndicus() {
-      // TODO syndicus toevoegen aan de backend + error handling + terug naar de vorige pagina
-      console.log(this.syndicus)
+    async addSyndicus() {
+      if (this.syndicus.id === '' || this.syndicus.location === '' || this.buildings.length === 0) {
+        this.$store.dispatch("snackbar/open", {
+          message: "Een van de verplichte velden is leeg.",
+          color: "error"
+        })
+        return;
+      }
+      for (let building_id of this.syndicus.buildings) {
+        let syndicus = this.allBuildings.filter(building => building.id === building_id)[0].syndicus
+        syndicus.push(this.syndicus.id)
+        await RequestHandler.handle(BuildingService.updateBuildingById(Number(building_id), {
+          'syndicus': syndicus
+        }), {
+          id: 'addSyndicusToBuilding',
+          style: 'SNACKBAR',
+          customMessages: [
+            {
+              code: '500',
+              message: 'Kon de syndicus niet toevoegen aan het gebouw.',
+              description: 'Kon de syndicus niet toevoegen aan het gebouw.'
+            }
+          ]
+        })
+      }
+      await RequestHandler.handle(AuthService.updateRoleOfUser({
+        email: this.allUsers.filter(user => user.id === this.syndicus.id)[0].email,
+        role: 'SY'
+      }), {
+        id: 'addSyndicusRole',
+        style: 'SNACKBAR',
+        customMessages: [
+          {
+            code: '500',
+            message: 'Kon de role van de user niet aanpassen.',
+            description: 'Kon de role van de user niet aanpassen.'
+          }
+        ]
+      })
+      console.log(this.$router.back())
     },
     editSyndicus() {
       // TODO syndicus aanpassen in de backend + error handling + terug naar de vorige pagina
@@ -107,7 +145,11 @@ export default {
         }
       ]
     }).then(users => {
-      this.allUsers = users.filter(x => x.role === 'AA').map(user => user['name'] = `${user.first_name} ${user.last_name}`)
+      this.allUsers = users.filter(x => x.role === 'AA').map(user => {
+        user['name'] = `${user.first_name} ${user.last_name}`
+        return user
+      })
+      console.log(this.allUsers)
     })
     RequestHandler.handle(LocationService.getLocations(), {
       id: 'getLocationsError',
