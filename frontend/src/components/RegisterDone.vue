87aeac856398f3a71d749cadff612f67c16c4f57
Arthur Deruytter <art.deruytter@gmail.com>
2023-04-20 20:36:20 +0200

    revert RegisterDone component

--- frontend/src/components/RegisterDone.vue
+++ frontend/src/components/RegisterDone.vue
@@ -5,15 +5,37 @@ additional information for the user that just registered.
 -->
 
 <template>
-  <trash-container-template-list/>
+  <v-container align="center">
+    <v-col cols="12" sm="6" md="6">
+      <v-card class='px-4'>
+        <v-card-title class="text-center">
+          <v-icon
+            color="success"
+            icon="mdi-check-circle"
+            size="x-large"
+          ></v-icon>
+        </v-card-title>
+        <v-spacer></v-spacer>
+        <v-card-text>
+          <v-row class="mx-auto justify-center">
+            <div class="mx-1">Registratie is gelukt!</div>
+          </v-row>
+          <v-row class="my-4 justify-center">
+            <div class="mx-1">Bedankt voor het registreren bij Dr. Trottoir.</div>
+          </v-row>
+          <v-row class="mb-4 justify-center">
+            <div class="mx-1">Als uw aanmelding is verwerkt, zal de applicatie hier beschikbaar zijn.</div>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
-import TrashContainerTemplateList from '@/components/containerTemplates/TrashContainerTemplateList.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'RegisterDone',
-  components: { TrashContainerTemplateList }
+  name: 'RegisterDone'
 })
 </script>
