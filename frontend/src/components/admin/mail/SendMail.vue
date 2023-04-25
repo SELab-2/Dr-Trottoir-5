f1f75f8e71f367f6e5a82ecb7a89fc8d8530be70
jbvilla <jonasbyle12@gmail.com>
2023-04-25 09:48:50 +0200

    verwerking templates is af

--- frontend/src/components/admin/mail/SendMail.vue
+++ frontend/src/components/admin/mail/SendMail.vue
@@ -16,14 +16,12 @@
       <div v-if="this.inputArguments.length !== 0">
         <h2>Argumenten</h2>
         <div v-for="(arg, index) in this.positionsArguments">
-          <label>{{getArgumentName(arg)}}</label>
+          <label>{{arg.substring(1, arg.length-1)}}</label>
           <v-text-field v-model="this.inputArguments[index]"></v-text-field>
         </div>
       </div>
       <h2>Bericht</h2>
-      <v-container style="white-space: pre-wrap; font-size: 16px" class="container-border">
-        <p> {{ this.description }} </p>
-      </v-container>
+      <v-container v-html="formattedText"  style="white-space: pre-wrap; font-size: 16px" class="container-border"/>
     </div>
     <v-container align="center">
       <NormalButton text="Stuur email" :parent-function="sendMail"/>
@@ -52,7 +50,8 @@ export default {
   data: () => ({
     description: null,
     templates: [
-      {id: 1, name: 'Template 1', description: 'Template 1#arg1#     #arg2#'},
+      {id: 1, name: 'Template 1', description: 'Beste #arg1#,\n\n#arg2#  #arg1#  dit is echt een mega lange text om te testen of het wel goed gaat, maar' +
+      'maar blijkbaar\ntesten of het wel goed gaat, maar\n\nMet vriendelijke groeten #naam#'},
       {id: 2, name: 'Template 2', description: 'Template 2'},
 
     ],
@@ -61,20 +60,19 @@ export default {
   }),
   methods: {
     sendMail() {
-      console.log(this.description);
-      console.log(this.inputArguments);
+      console.log(this.getDescriptionWithArguments());
     },
     updateArguments() {
       this.positionsArguments = [];
 
-      const regex = /#(\w+)#/g;
+      const regex = /(#\w+#)/g;
       let match;
 
       while ((match = regex.exec(this.description)) !== null) {
-        this.positionsArguments.push({
-          position: match.index,
-          length: match[1].length
-        });
+        const argument = match[1];
+        if (!this.positionsArguments.includes(argument)){
+          this.positionsArguments.push(argument);
+        }
       }
       if (this.positionsArguments.length !== 0) {
         this.inputArguments = new Array(this.positionsArguments.length).fill('');
@@ -82,14 +80,28 @@ export default {
         this.inputArguments = [];
       }
     },
-    getArgumentName(arg) {
-      return this.description.substring(arg.position+1, arg.position + arg.length+1);
+    getDescriptionWithArguments() {
+      let description = this.description;
+      for (let i = 0; i < this.positionsArguments.length; i++) {
+        if (this.inputArguments[i] !== '') {
+          description = description.replaceAll(this.positionsArguments[i], this.inputArguments[i]);
+        }
+      }
+      return description;
     }
   },
   watch: {
     description(){
       this.updateArguments();
     }
+  },
+  computed: {
+    formattedText() {
+      return this.getDescriptionWithArguments().replace(/#([^#]*)#/g, '<b>$1</b>');
+    }
+  },
+  mounted() {
+    //TODO get templates from backend
   },
   components: {NormalButton, FotoCardSyndicus}
 }
