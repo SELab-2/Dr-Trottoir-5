26cfa121fedd98444cdf8b7e2ae5a9882bfe4fe5
jbvilla <jonasbyle12@gmail.com>
2023-05-03 16:46:06 +0200

    DOMWrapper kunnen gebruiken inplaats van setValue

--- frontend/tests/unit/Components/admin/mail/mailTests.spec.js
+++ frontend/tests/unit/Components/admin/mail/mailTests.spec.js
@@ -3,13 +3,27 @@ import CreateEditMailTemplate from '@/components/admin/mail/CreateEditMailTempla
 
 describe('CreateEditMailTemplate.vue', () => {
 
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(CreateEditMailTemplate);
+  })
+
+
   it('Render title create', () => {
-    const wrapper = mount(CreateEditMailTemplate, {
-      props: {
-        edit: false
-      }
-    })
     expect(wrapper.find('h1').text()).toMatch('Mail template aanmaken')
+  })
+
+  it('Render title edit', async () => {
+    wrapper.setProps({ edit: true })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('h1').text()).toMatch('Mail template aanpassen')
+  })
+
+  it('sets the v-model of v-text-field for the name of the mail template',  () => {
+    const textField = wrapper.find('v-text-field')
+    textField.element.value = 'test';
+    textField.trigger('input');
   })
 
 
