764500440ae7ca7c4bf052883ad0872e38a409fc
pjotrvisman <vismanpjotr@gmail.com>
2023-05-04 20:56:58 +0200

    attempt to fix action runner

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
       }
     }
   },
-  ignorePatterns: ['node_modules/'],
+  ignorePatterns: ['**/node_modules/**'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
