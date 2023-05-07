4e6b0b3573a91e86b41a54ee7caf8b7884b700eb
jbvilla <jonasbyle12@gmail.com>
2023-05-07 18:13:45 +0200

    verder testen proberen fixen

--- frontend/tests/unit/services/MailTemplateServiceTest.spec.ts
+++ frontend/tests/unit/services/MailTemplateServiceTest.spec.ts
@@ -1,30 +1,40 @@
 import nock from "nock";
 import MailTemplateService from "@/api/services/MailTemplateService";
 import MailTemplate from "@/api/models/MailTemplate";
-import { InputFields } from "@/types/fields/InputFields";
+import {EchoPromise, EchoService, EchoServiceBuilder, POST} from "@/api/EchoFetch";
 
 const MOCK_SERVER_URL = "http://localhost:8000/api";
-jest.mock("@/api/services/MailTemplateService");
+
+class TestService extends EchoService {
+    @POST("/mailtemplates")
+    public post(): EchoPromise<MailTemplate> {
+        return <EchoPromise<MailTemplate>>{}
+    };
+}
+
+function setupService(): TestService {
+    return new EchoServiceBuilder()
+        .setBaseUrl(MOCK_SERVER_URL)
+        .build(TestService);
+}
+
 describe('MailTemplateService', () => {
   afterEach(() => {
     nock.cleanAll();
   });
 
   it('createMailTemplate', async () => {
-    MailTemplateService.createMailTemplate = jest.fn();
-  //MailTemplateService.createMailTemplate.mockResolvedValue(new MailTemplate());
-    const template = new InputFields();
-
     const expectedResponse = new MailTemplate();
     expectedResponse.id = 1;
     expectedResponse.name = 'test-template';
     expectedResponse.content = 'test';
 
     const scope = nock(MOCK_SERVER_URL)
-      .post('/mailtemplates', template)
+      .post('/mailtemplates')
       .reply(200, expectedResponse);
 
-    const response = MailTemplateService.createMailTemplate(template);
+    const service = setupService();
+    const response = await service.post();
 
     expect(response).toEqual(expectedResponse);
     expect(scope.isDone()).toBeTruthy();
