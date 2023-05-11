1c5b3c2ab8e11c86a723074066885997185fd2af
pjotrvisman <vismanpjotr@gmail.com>
2023-05-11 16:11:42 +0200

    update syndicus dashboard

--- frontend/src/views/syndicus/SyndicusHome.vue
+++ frontend/src/views/syndicus/SyndicusHome.vue
@@ -5,7 +5,7 @@
         <v-row>
           <v-col md="6" sm="6">
             <DatePicker v-model.string="date" color="white" :is-dark="true" :is-required="true" show-iso-weeknumbers
-                        :first-day-of-week="1" :masks="masks" :attributes="attrs" view="weekly" />
+                        :first-day-of-week="1" :masks="masks" :attributes="attrs" view="weekly" v-on:dayclick="changed" />
           </v-col>
           <v-col sm="6" align="left" class="text-wrap">
             <h4 class="text-h4">
@@ -15,12 +15,29 @@
             <v-autocomplete
               label="Gebouw" :items="buildings" class="mt-4" style="width: 50%;"
               v-model="building" item-title="name" return-object
-              variant="outlined"
+              variant="outlined" v-on:update:modelValue="changed"
             ></v-autocomplete>
           </v-col>
         </v-row>
       </v-card-title>
       <v-divider style="width: 90%;" class="mt-4"></v-divider>
+       <v-card-text class="mt-3">
+        <v-row justify="center">
+          <v-col cols="4">
+            <v-row justify="center" class="text-center">
+              <v-col cols="12"><h1>Aankomst</h1></v-col>
+              <br>
+              <ul>
+                <li v-for="(el) in this.arrivals" :key="el">
+                  <v-col cols="12">
+                    <FotoCardAdmin v-bind:data="el"/>
+                  </v-col>
+                </li>
+              </ul>
+            </v-row>
+          </v-col>
+        </v-row>
+       </v-card-text>
      </v-card>
   </v-container>
 </template>
@@ -30,13 +47,19 @@ import { DatePicker } from 'v-calendar';
 import 'v-calendar/dist/style.css';
 import {RequestHandler} from "@/api/RequestHandler";
 import RoundService from "@/api/services/RoundService";
+import FotoCardAdmin from "@/components/admin/FotoCardAdmin.vue";
+import { getWeek } from "@/api/DateUtil";
+import PlanningService from "@/api/services/PlanningService";
 
 export default {
   name: "SyndicusHome",
-  components: {DatePicker},
+  components: {FotoCardAdmin, DatePicker},
   data: () => ({
     date: new Date().toISOString().split('T')[0],
     buildings: [],
+    arrivals: [],
+    departs: [],
+    storages: [],
     building: null,
     masks: { modelValue: 'YYYY-MM-DD' },
     attrs: [
@@ -64,6 +87,63 @@ export default {
       this.buildings = buildings;
       if (buildings.length > 0) this.building = buildings[0];
     }).catch(() => null);
+  },
+  methods: {
+    changed() {
+      this.getStudentPosts();
+    },
+    async getStudentPosts(){
+      const date = new Date(this.date)
+      let week = getWeek(date)
+      if (date.getUTCDay() === 0) week -= 1;
+      await RequestHandler.handle(PlanningService.getRounds(date.getFullYear(), week, date.getUTCDay(), this.building.location), {
+        id: 'getRoundsError',
+        style: 'NONE'
+      }).then(async rounds => {
+        console.log(rounds, this.building.id)
+        for(const round of rounds){
+          if(round.ronde.buildings.map(building => building.id).includes(this.building.id)){
+            console.log('INCLUDES!')
+            await RequestHandler.handle(PlanningService.getInfoOfBuilding(round.id, this.building.id), {
+              id: 'getInfoOfBuildingError',
+              style: 'NONE'
+            }).then(async info => {
+              console.log(info)
+              await RequestHandler.handle(PlanningService.getPictures(info[0].id, date.getFullYear(), week), {
+                id: 'getPicturesError',
+                style: 'NONE'
+              }).then(async pictures => {
+                console.log(pictures)
+                this.arrivals = []
+                this.departs = []
+                this.storages = []
+                for(const picture of pictures){
+                  if(picture.pictureType === "AR"){
+                    this.arrivals.push(picture)
+                  } else if(picture.pictureType === "DE"){
+                    this.departs.push(picture)
+                  } else if(picture.pictureType === "ST"){
+                    this.storages.push(picture)
+                  }
+                }
+              }).catch(() => {
+                this.arrivals = []
+                this.departs = []
+                this.storages = []
+              })
+            }).catch(() => {
+              this.arrivals = []
+              this.departs = []
+              this.storages = []
+            })
+          }
+        }
+      }).catch(() => {
+        this.arrivals = []
+        this.departs = []
+        this.storages = []
+      });
+    }
   }
 }
 </script>
