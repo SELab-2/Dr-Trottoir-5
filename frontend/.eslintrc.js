1d098a5473cc8994d4c4a8e00e07b3e8c82f189f
pjotrvisman <vismanpjotr@gmail.com>
2023-05-14 10:59:38 +0200

    Action runner linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,14 +5,15 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    '@vue/standard',
+    '@vue/typescript/recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    camelcase: 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off'
   },
   overrides: [
     {
