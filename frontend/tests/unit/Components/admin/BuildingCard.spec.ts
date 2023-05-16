8bc5f33d3391395244650c4f130f953fe486ff59
Kai Hozee <kaihozee@icloud.com>
2023-05-16 17:09:42 +0200

    BuildingCard error undefined

--- frontend/tests/unit/Components/admin/BuildingCard.spec.ts
+++ frontend/tests/unit/Components/admin/BuildingCard.spec.ts
@@ -5,27 +5,23 @@ describe('BuildingCard.vue', () => {
 
   let wrapper
 
-  it('temp', () => {})
+  const data = {
+    name: 'Test Gebouw',
+    id: 1,
+    adres: 'TestStraat 1',
+    ivago_klantnr: 1,
+    buildingID: "test",
+    manual: 1,
+  }
+  beforeEach(async () => {
+    BuildingCard.beforeMount = () => Promise.resolve();
+    wrapper = mount(BuildingCard, {
+      props: data
+    });
+  });
 
-  // const data = {
-  //   name: 'Test Gebouw',
-  //   id: 1,
-  //   adres: 'TestStraat 1',
-  //   ivago_klantnr: 1,
-  //   buildingID: "test",
-  //   manual: 1,
-  // }
-  // beforeEach(async () => {
-  //   BuildingCard.beforeMount = () => Promise.resolve();
-  //   wrapper = mount(BuildingCard, {
-  //     props: data
-  //   });
-  //   await wrapper.setProps(data);
-  //   console.log(wrapper.vm.$props);
-  // });
-  //
-  // it('render of component', async () => {
-  //   expect(wrapper.exists()).toBeTruthy();
-  // })
+  it('render of component', async () => {
+    expect(wrapper.exists()).toBeTruthy();
+  })
 
 })
